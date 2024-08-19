d82d8df02bb932baa47b66fb0efcdfc43bf148cc
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-19 11:41:18 +0000

    updated CSRF settings to allow access to domain

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,9 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
 
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
+
+
 # Application definition
 
 INSTALLED_APPS = [
