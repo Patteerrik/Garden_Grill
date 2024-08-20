0dbb65baf130155307bbaca85e88e7712c646ea3
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-20 09:54:41 +0000

    changed typo in settings.py

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-n=$xvn#j_9t*l!ri!)2_6++o0!0*w&q0)$6=6-#h1x+3*9+l83')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
