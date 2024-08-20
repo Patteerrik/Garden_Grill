861461541d69f1f33d21cb156e218fcad3ad4136
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-20 10:31:04 +0000

    update settings for environment variables

--- main/settings.py
+++ main/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
