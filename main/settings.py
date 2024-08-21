eb6aa85cf2ee04c7b35639fedf077f68aec36ca7
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-21 09:24:12 +0000

    add change in env and setting to prepare for heroku deployment

--- main/settings.py
+++ main/settings.py
@@ -14,7 +14,7 @@ import os
 import environ
 from pathlib import Path
 import dj_database_url
-from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
