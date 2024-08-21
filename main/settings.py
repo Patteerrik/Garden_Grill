a180dfd17de0c9bba5848bba761dc5b4148e6068
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-21 10:46:57 +0000

    removed typo

--- main/settings.py
+++ main/settings.py
@@ -33,8 +33,6 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-if DEBUG:
-
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
