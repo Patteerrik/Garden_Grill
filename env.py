7983a077b9d7578ab99dbe0f3c36db37fcb42d52
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-22 12:51:09 +0000

    removed env.py from repository

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-os.environ.setdefault("SECRET_KEY", "nckdwnqcöklwdnqklöcnwkdqnckwniwncjkqwbcjkwqebnvclkj")
-os.environ.setdefault("DATABASE_URL", "postgres://u1mogaafdgm:yLVic0M2beri@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gong_spot_zone_899390")
-os.environ.setdefault("DEBUG", "True")
-
-
