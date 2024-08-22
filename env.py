41f732d9c4fdbb32f070e0463931ef359b937447
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-22 12:31:21 +0000

    updated setting.py and added env.py for enviroment variables

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "ybl$y@3u&oe4u%jao1-gn4l2w*npr1ml%ipmxh4vemydyc1@s")
+os.environ.setdefault("DATABASE_URL", "postgres://u1mogaafdgm:yLVic0M2beri@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/gong_spot_zone_899390")
+os.environ.setdefault("DEBUG", "True")
+
+
