93359231699b2a34b10a7a48fdb1e4df1de97a38
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-26 08:05:07 +0000

    secured api key and fixed google maps display

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    context = {
+        'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY
+    }
+    return render(request, 'home/index.html', context)
 
 def hello(request):
     return render(request, 'home/hello.html')
