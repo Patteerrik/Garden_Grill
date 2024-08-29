cd34ff6d544d8bda40be7b4a204abf9600b7b398
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-08-29 09:47:59 +0000

    updated views.py and urls to allow admins to update and cancel in one view

--- reservations/urls.py
+++ reservations/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('create/', views.create_reservation, name='create_reservation'),
     # path('list/', views.list_reservations, name='list_reservations'),
     path('update/<int:pk>/', views.update_reservation, name='update_reservation'),
-    path('cancel/<int:pk>/', views.cancel_reservation, name='cancel_reservation'),
+    # path('cancel/<int:pk>/', views.cancel_reservation, name='cancel_reservation'),
     path('admin/bookings/', views.booking_management, name='booking_management')
-    
+
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Reservation
 from .forms import ReservationForm
+
+def is_admin_user(user):
+    return user.is_authenticated and user.is_admin
+
+@login_required
+@user_passes_test(is_admin_user)
 
 def update_reservation(request, pk):
     reservation = get_object_or_404(Reservation, pk=pk)
     form = ReservationForm(request.POST or None, instance=reservation)
 
-    if 'update' in request.POST:
-        if form.is_valid():
-            form.save()
-            return redirect('list_reservations')
+    if 'update' in request.POST and form.is_valid():
+        form.save()
+        return redirect('list_reservations')
     elif 'cnacel' in request.POST:
         reservation.delete()
         return redirect('list_reservations')
