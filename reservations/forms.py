72b0a5a11369b73b94093c9d41068d89959e1b06
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-09-30 11:16:41 +0000

    Added function to deny bookings outside opening hours

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 from .models import Reservation
+from datetime import time
+
+
+# Define opening hours
+OPENING_TIME = time(12, 0)  # 12:00 PM
+CLOSING_TIME = time(22, 0)  # 10:00 PM
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['reservation_name', 'date', 'time', 'number_of_guests']  
+
+    
+    def clean_time(self):
+        reservation_time = self.cleaned_data.get('time')
+
+        # Check time is beetween 12:00 PM and 10:00 PM
+        if not (OPENING_TIME <= reservation_time <= CLOSING_TIME):
+            raise forms.ValidationError("The reservation time must be between 12:00 PM and 10:00 PM.")
+
+        return reservation_time
