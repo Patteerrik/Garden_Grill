9267dcb8bc7d652bbd4eab77872327c91d555967
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-10-02 13:02:29 +0000

    Updated forms to prevent past time/date bookings

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Reservation
-from datetime import time
+from datetime import time, datetime, date
 from django.contrib.auth import get_user_model
 
 
@@ -15,12 +15,27 @@ class ReservationForm(forms.ModelForm):
         fields = ['reservation_name', 'date', 'time', 'number_of_guests']  
 
     
+    def clean_date(self):
+        reservation_date = self.cleaned_data.get('date')
+        today = date.today()
+        
+        if reservation_date < today:
+            raise forms.ValidationError("The reservation date cannot be in the past.")
+        
+        return reservation_date
+    
     def clean_time(self):
         reservation_time = self.cleaned_data.get('time')
+        reservation_date = self.cleaned_data.get('date')
+        current_time = datetime.now().time()
 
         # Check time is beetween 12:00 PM and 10:00 PM
         if not (OPENING_TIME <= reservation_time <= CLOSING_TIME):
             raise forms.ValidationError("The reservation time must be between 12:00 PM and 10:00 PM.")
+
+        if reservation_date == date.today() and reservation_time < current_time:
+            raise forms.ValidationError("The reservation time cannot be in the past.")
+
 
         return reservation_time
 
