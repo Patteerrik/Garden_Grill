5810b6b78d293177d81faa11731f64fca4ecda77
Patrik Ãkerberg <patrik.akerbergz@gmail.com>
2024-10-11 12:09:39 +0000

    Did automatic testing fo reservation form

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from reservations.forms import ReservationForm
 
-# Create your tests here.
+class TestReservationForm(TestCase):
+    # This is a test class for the ReservationForm
+
+    def test_form_is_valid(self):
+        # Test case to check if the form is valid with correct data
+        form_data = {
+            # Valid inputs for test
+            'reservation_name': 'Test User',
+            'date': '2024-12-01',
+            'time': '18:00',
+            'number_of_guests': 2
+        }
+
+        # Fill the ReservationForm with the form_data
+        reservation_form = ReservationForm(form_data)
+        # Check if the form is valid with the provided data
+        self.assertTrue(reservation_form.is_valid(), msg="Form is invalid")
