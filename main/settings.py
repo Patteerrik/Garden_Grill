4aa8a405dd3653cfc5651aafd09a0bd1be8a59f3
Patrik Åkerberg <patrik.akerbergz@gmail.com>
2025-02-12 15:22:14 +0100

    fix: restored files and optimizations

--- main/settings.py
+++ main/settings.py
@@ -106,7 +106,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-
 ]
 
 # Crispy forms settings
